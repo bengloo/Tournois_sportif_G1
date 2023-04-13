3027815b078c4e14d2a79e6d272b6d551e0af3ac
Benjamin Engloo <engloo.benjamin@gmail.com>
2023-04-13 22:42:49 +0200

    test championat

---
+++ src/Tests/TestChampionnat.java
@@ -0,0 +1,27 @@
+package Tests;
+
+import instance.Instance;
+import io.InstanceReader;
+import io.JourneeReader;
+import io.exception.ReaderException;
+import solution.Championnat;
+import solution.Journee;
+
+import java.util.HashMap;
+
+public class TestChampionnat {
+    public static void main(String[] args) {
+        String path="instances/instance_ITC2021_Test_4.txt";
+        try {
+            InstanceReader reader = new InstanceReader(path);
+            JourneeReader readerJ = new JourneeReader(path);
+            HashMap<Integer, Journee> journees = new HashMap<>();
+            journees= readerJ.readJournees();
+            Instance i= reader.readInstance(journees);
+            Championnat c= new Championnat(i,journees);
+            System.out.println("Instance lue avec success !");
+        } catch (ReaderException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
