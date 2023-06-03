f51f507c835c4f0790fc04565a631146498eceee
Piroline <75422806+Piroline@users.noreply.github.com>
2023-06-03 16:11:58 +0200

    Pimp de l'interface ✨
    + ajout script Powershell + fichier .jar + dossier solutions

---
+++ Run_program.ps1
@@ -0,0 +1,22 @@
+<# java -jar Tournois_sportif_G1.jar $args[0] #>
+Write-Host "Execution Done! Check our webapp!!!!!!"
+<# Read-Host -Prompt "Press Enter to exit" #>
+<#
+	public static String inFile;
+	
+	readArgs(args);
+	
+	public static void readArgs(String args[]) {
+        if (args.length < 1) {
+            printUsage();
+            System.exit(-1);
+        }
+
+        int index = -1;
+
+        inFile = args[++index];
+        }
+    public static void printUsage() {
+        System.out.println("Usage: java -jar Tournois_sportif_G1.jar <input>");
+    }
+#>
---
+++ output.php
@@ -0,0 +1,93 @@
+<!-- Source: https://www.bootdey.com/snippets/view/bs4-event-timeline -->
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="jquery-3.7.0.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+<link rel='stylesheet' type='text/css' href='./style_output.css'>
+
+<div class="container animated animated-done bootdey" data-animate="fadeIn" data-animate-delay="0.05" style="animation-delay: 0.05s;">
+    <input type="file" name="readfile" id="readfile" />
+
+    <hr class="hr-lg mt-0 mb-2 w-10 mx-auto hr-primary">
+    <div class="timeline timeline-left mx-lg-10">
+      
+    </div>
+<div> 
+    
+<!-- <input type="file" id="fileInput">
+  <button onclick="getFileName()">Obtenir le nom du fichier</button> -->
+
+
+<script>
+    const fileName = '';
+    let file = document.getElementById("readfile");
+    
+    file.addEventListener("change", function () {
+        var reader = new FileReader();
+        reader.onload = function (progressEvent) {
+            var lines = this.result.split('\n'); // Divise le contenu en lignes
+            var journees = []; // Tableau pour stocker les journées
+            var journee = []; // Tableau pour stocker les lignes d'une journée
+            var i = 0;
+            
+            lines.forEach(function(line) {
+                if (line.trim() !== '' && !line.startsWith("//")) { // Vérifie si la ligne n'est pas vide
+                    var values = line.split('   '); // Divise la ligne en valeurs (suppose que les valeurs sont séparées par une tabulation)
+                    $('#box-'+(i-1)).append('<div class="timeline-item mt-3 row text-center p-2">'+
+                        '<div class="col font-weight-bold text-md-right">'+values[0]+'</div>'+
+                        '<div class="col-1">vs</div>'+
+                        '<div class="col font-weight-bold text-md-left">'+values[1]+'</div>'+
+                        '<div class="col-12 text-xs text-muted"></div>'+
+                    '</div>');
+                    journee.push(values.map(Number)); // Ajoute la ligne sous forme de tableau de nombres dans la journée
+                } else if (!line.startsWith("//")) {
+                    $('.timeline').append('<div class="box" id="box-'+i+'">'+
+                                            '<div class="timeline-breaker">Journée '+i+'</div>'+
+                                            '</div>');
+                    journees.push(journee); // Ajoute la journée au tableau des journées
+                    journee = []; // Réinitialise le tableau pour la prochaine journée
+                    i++;
+                }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+            });
+            // Vérifie s'il reste des lignes non ajoutées comme une journée finale
+            if (journee.length > 0) {
+                journees.push(journee);
+            }
+
+            // Supprime le 1er élément du tableau car il n'y a pas encore eu de journée parcourue à ce stage
+            journees.shift();
+
+            // Affiche le tableau des journées dans la console
+            console.log(journees);
+        };
+        reader.readAsText(this.files[0]);
+    });
+
+
+
+
+  /*function getFileName() {
+    const fileInput = document.getElementById('fileInput');
+    fileName = fileInput.files[0].name;
+    alert("Nom du fichier sélectionné : " + fileName);
+  }*/
+
+
+</script>
+
+
+
+
+<?php
+if (isset($_GET['run']) && $_GET['run'] == true) {
+    # This code will run if ?run=true is set.
+    echo "<pre>";
+    echo Shell_Exec('powershell -InputFormat none -ExecutionPolicy ByPass -NoProfile -Command "& { . \".\Run_program.ps1\ "; }"');
+    echo "</pre>";
+}   
+?>
+
+<!-- This link will add ?run=true to your URL, myfilename.php?run=true -->
+<a href="?run=true">Click Me!</a>
+  
