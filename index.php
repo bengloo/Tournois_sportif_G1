06cca42cf1b2c2f26009c0cae52d4b05b4ad8199
Piroline <75422806+Piroline@users.noreply.github.com>
2023-06-11 19:35:12 +0200

    Améliorations de index.php
    - Nettoyer le code
    - Indiquer le nom du fichier importé avant le clic sur "Soumettre"
    - Gérer le cas où on importe un fichier différent d'un .txt ou rien du tout
    - Trouver un beau design de page de chargement et le simuler pour qq secondes données
    - Supprimer execution_done quand on arrive sur output.php

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 <!DOCTYPE html>
 <meta charset="utf-8">
 
-<title> Visualisation des solutions </title>
+<title>Tournoi sportif G1  - Import instance</title>
 <html>
     <head>
         <link rel="stylesheet" href="./style.css">
+        <script src="jquery-3.7.0.js"></script>
     </head>
     <body>
         <div class="frame" id="content">
@@ -17,6 +18,7 @@
 
                 <div class="dropzone">
                     <img src="http://100dayscss.com/codepen/upload.svg" class="upload-icon"/>
+                    <span class="filename"></span>
                     <input type="file" class="upload-input" id="fileInput"/>
                 </div>
                 <form id="myForm" method="post" action="">
@@ -30,83 +32,70 @@
 
 <script>
 
+$(document).ready(function() {
+    // Écoute de l'événement "change" sur l'input file
+    $('#fileInput').on('change', function(e) {
+        // Récupérer le fichier sélectionné
+        fileName = e.target.files[0].name;
+
+        // Remplacer l'icône d'import par le nom du fichier
+        $('.filename').html(fileName);
+        $('.dropzone .upload-icon').hide();
+
+        // Réglage de la position de l'input file selon la longueur du nom de fichier
+        if ($('.filename').height() > 16) {
+            $('.upload-input').css('top', '-41px');
+        } else {
+            $('.upload-input').css('top', '-21px');
+        }
+    });
+});
+
 function getFileName() {
     const fileInput = document.getElementById('fileInput');
-    fileName = fileInput.files[0].name;
+    
+    if (fileInput.files[0] != null) {
+        fileName = fileInput.files[0].name;
+        return fileName;
+    } else {
+        return null;
+    }
 }
 
 
 function submitForm() {
-    getFileName();
+    let counter = 0;
+    fileName = getFileName();
+    console.log(fileName);
 
-    // Remplir un champ de formulaire invisible avec la valeur de la variable JavaScript
-    document.getElementById('jsVariableInput').value = fileName;
+    if (fileName != null && fileName.split('.').pop() == "txt") {
+        // Remplir un champ de formulaire invisible avec la valeur de la variable JavaScript
+        document.getElementById('jsVariableInput').value = fileName;
 
-    // Soumettre le formulaire
-    document.getElementById('myForm').submit();
+        // Soumettre le formulaire
+        document.getElementById('myForm').submit();
+    } else {
+        $('#attenteImport').css('color', 'red');
+        $('#attenteImport').html("Erreur : aucun fichier n'a été importé ou le fichier n'est pas au format .txt");
+        const interval = setInterval(function() {
+            $('#attenteImport').fadeToggle(500);
+            counter += 500;
+            if (counter >= 2000) {
+                clearInterval(interval);
+            }
+        }, 500);
+    }
 }
 
-////////////////////////////////////////////////////////////////////////////////  Code qui affiche un menu déroulant affichant les fichiers d'un dossier
 // TODO: dès que le fichier execution_done a été créé, on va 
-// lister tous les fichiers dans un menu (ou prendre le + récent?)
-
-<!DOCTYPE html>
-<html>
-<head>
-  <style>
-    /* Style du menu déroulant */
-    select {
-      width: 300px;
-      padding: 5px;
-      font-size: 16px;
-    }
-  </style>
-</head>
-<body>
-  <label for="fileSelect">Sélectionnez un fichier :</label>
-  <select id="fileSelect"></select>
-
-  <script>
-    // Fonction pour charger les fichiers d'un dossier
-    function loadFiles() {
-      var fileSelect = document.getElementById("fileSelect");
-      var folderPath = "chemin/vers/votre/dossier"; // Remplacez cela par le chemin vers votre dossier
-
-      // Appel à une API côté serveur pour obtenir la liste des fichiers dans le dossier
-      // Ici, nous utilisons l'API Fetch pour effectuer une requête AJAX
-      fetch("/get-files?folderPath=" + encodeURIComponent(folderPath))
-        .then(response => response.json())
-        .then(data => {
-          // Parcourt les fichiers retournés et ajoute-les en tant qu'options du menu déroulant
-          data.forEach(file => {
-            var option = document.createElement("option");
-            option.value = file;
-            option.text = file;
-            fileSelect.appendChild(option);
-          });
-        })
-        .catch(error => {
-          console.log("Une erreur s'est produite lors du chargement des fichiers :", error);
-        });
-    }
-
-    // Appel de la fonction pour charger les fichiers au chargement de la page
-    window.onload = loadFiles;
-  </script>
-</body>
-</html>
-// Ce code crée un menu déroulant (select) qui sera rempli avec les noms de fichiers retournés par l'API côté serveur. 
-// L'API côté serveur est responsable de récupérer la liste des fichiers dans le dossier spécifié. 
-// Notez que cet exemple suppose que vous disposez d'une API côté serveur pour récupérer la liste des fichiers dans le dossier spécifié. La mise en place de cette API dépend du langage et du framework que vous utilisez côté serveur.
-
-
-//////////////////////////////////////////////
+// rediriger vers output.php et y lister tous les fichiers dans un menu
 
 window.addEventListener('DOMContentLoaded', function() {
     // Vérifier périodiquement la présence du fichier
     setInterval(checkFilePresence, 1000);
 });
 
+// Vérifie la présence d'un fichier
 function checkFilePresence() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
@@ -116,25 +105,28 @@ function checkFilePresence() {
                 window.location.href = "./output.php";
             } else {
                 // Le fichier n'est pas encore présent
-                document.getElementById('content').innerHTML = "<p>Le fichier n'est pas encore présent.</p>";
+                if ($('#attenteImport').length == 0) {
+                    $('.center').append("<div id='attenteImport'>Veuillez importer un fichier d'instance au format .txt</div>");
+                }
             }
         }
     };
-    xhr.open('HEAD', './solutions/execution_done.ps1', true);
+    xhr.open('HEAD', './solutions/execution_done', true);
     xhr.send();
 }
 </script>
 
 <?php
+    // Permet de déclencher le script Powershell pour exécuter notre application Java
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['fileName'])) {
         // Récupérer la valeur de la variable JavaScript envoyée via le formulaire
         $fileName = $_POST['fileName'];
-
+        echo '<div id="loadingOverlay"><div class="loadingSpinner"></div></div>';
         echo "La valeur de la variable JavaScript est : " . $fileName;
         
         $commande = 'powershell -InputFormat none -ExecutionPolicy ByPass -NoProfile -Command "& { .\Run_program.ps1 ' . $fileName;
         $commande = $commande . '; }"';
-    // echo $commande;
+        // echo $commande;
         echo "<pre>";
         echo Shell_Exec($commande);
         echo "</pre>";
--- output.php
+++ output.php
@@ -1,30 +1,72 @@
 <!-- Source: https://www.bootdey.com/snippets/view/bs4-event-timeline -->
 
+<!DOCTYPE html>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<script src="jquery-3.7.0.js"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
-<link rel='stylesheet' type='text/css' href='./style_output.css'>
 
-<div class="container animated animated-done bootdey" data-animate="fadeIn" data-animate-delay="0.05" style="animation-delay: 0.05s;">
-    <input type="file" name="readfile" id="readfile" />
+<title>Tournoi sportif G1 - Solution</title>
+<html>
+	<head>
+		<script src="jquery-3.7.0.js"></script>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+		<link rel='stylesheet' type='text/css' href='./style_output.css'>
+	</head>
+  
 
-    <hr class="hr-lg mt-0 mb-2 w-10 mx-auto hr-primary">
-    <div class="timeline timeline-left mx-lg-10">
-      
-    </div>
-<div> 
+	<div class="container animated animated-done bootdey" data-animate="fadeIn" data-animate-delay="0.05" style="animation-delay: 0.05s;">
+		<input type="file" name="readfile" id="readfile" />
+
+		<hr class="hr-lg mt-0 mb-2 w-10 mx-auto hr-primary">
+		<div class="timeline timeline-left mx-lg-10">
+		
+		</div>
+	<div>
+
+	<input type="file" id="fileInput">
+	<!-- <button onclick="getFileName()">Obtenir le nom du fichier</button> -->
+</html>
     
-
-
-<input type="file" id="fileInput">
-<!-- <button onclick="getFileName()">Obtenir le nom du fichier</button> -->
 
 
 <script>
-    fileName = '';
+	// TODO: supprimer execution_done
     let file = document.getElementById("readfile");
+
+	//////////////////////////////////////////////////////////////////////////////// 
+
+	// Ce code crée un menu déroulant (select) qui sera rempli avec les noms de fichiers retournés par l'API côté serveur. 
+	// L'API côté serveur est responsable de récupérer la liste des fichiers dans le dossier spécifié. 
+	// Notez que cet exemple suppose que vous disposez d'une API côté serveur pour récupérer la liste des fichiers dans le dossier spécifié. La mise en place de cette API dépend du langage et du framework que vous utilisez côté serveur. 
     
+	// Fonction pour charger les fichiers d'un dossier
+	function loadFiles() {
+		var fileSelect = document.getElementById("fileSelect");
+		var folderPath = "chemin/vers/votre/dossier"; // Remplacez cela par le chemin vers votre dossier
+
+		// Appel à une API côté serveur pour obtenir la liste des fichiers dans le dossier
+		// Ici, nous utilisons l'API Fetch pour effectuer une requête AJAX
+		fetch("/get-files?folderPath=" + encodeURIComponent(folderPath))
+		.then(response => response.json())
+		.then(data => {
+			// Parcourt les fichiers retournés et ajoute-les en tant qu'options du menu déroulant
+			data.forEach(file => {
+				var option = document.createElement("option");
+				option.value = file;
+				option.text = file;
+				fileSelect.appendChild(option);
+			});
+		})
+		.catch(error => {
+			console.log("Une erreur s'est produite lors du chargement des fichiers :", error);
+		});
+	}
+
+	// Appel de la fonction pour charger les fichiers au chargement de la page
+	window.onload = loadFiles;
+
+
+	////////////////////////////////////////////////////////////////////////////////
+
     file.addEventListener("change", function () {
         var reader = new FileReader();
         reader.onload = function (progressEvent) {
@@ -66,23 +108,21 @@
     });
 
 
-  function getFileName() {
-    const fileInput = document.getElementById('fileInput');
-    fileName = fileInput.files[0].name;
-  }
+	function getFileName() {
+		const fileInput = document.getElementById('fileInput');
+		fileName = fileInput.files[0].name;
+	}
 
 
-  function submitForm() {
-    getFileName();
-    
-    // Remplir un champ de formulaire invisible avec la valeur de la variable JavaScript
-    document.getElementById('jsVariableInput').value = fileName;
+	function submitForm() {
+		getFileName();
 
-    // Soumettre le formulaire
-    document.getElementById('myForm').submit();
-  }
+		// Remplir un champ de formulaire invisible avec la valeur de la variable JavaScript
+		document.getElementById('jsVariableInput').value = fileName;
 
-
+		// Soumettre le formulaire
+		document.getElementById('myForm').submit();
+	}
 
 </script>
 
@@ -95,19 +135,25 @@
 
 
 <?php
+	$fic = "./solutions/execution_done.txt";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['fileName'])) {
-    // Récupérer la valeur de la variable JavaScript envoyée via le formulaire
-    $fileName = $_POST['fileName'];
+	// Suppression du fichier execution_done pour qu'il n'interfère pas la prochaine fois qu'on ira sur index.php
+	if (file_exists($fic)) {
+		unlink($fic);
+	}
 
-    echo "La valeur de la variable JavaScript est : " . $fileName;
-    
-    $commande = 'powershell -InputFormat none -ExecutionPolicy ByPass -NoProfile -Command "& { .\Run_program.ps1 ' . $fileName;
-    $commande = $commande . '; }"';
-   // echo $commande;
-    echo "<pre>";
-    echo Shell_Exec($commande);
-    echo "</pre>";
-  }
+	if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['fileName'])) {
+		// Récupérer la valeur de la variable JavaScript envoyée via le formulaire
+		$fileName = $_POST['fileName'];
+
+		echo "La valeur de la variable JavaScript est : " . $fileName;
+		
+		$commande = 'powershell -InputFormat none -ExecutionPolicy ByPass -NoProfile -Command "& { .\Run_program.ps1 ' . $fileName;
+		$commande = $commande . '; }"';
+		// echo $commande;
+		echo "<pre>";
+		echo Shell_Exec($commande);
+		echo "</pre>";
+	}
 ?>
   
